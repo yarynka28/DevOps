e8a71f86a13068e90682847ecabee89b767b29b0
yarynka28 <104198926+yarynka28@users.noreply.github.com>
2023-01-02 16:23:41 +0200

    Add grafana.sh

---
+++ task6/grafana.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana_9.3.2_amd64.deb
+sudo dpkg -i grafana_9.3.2_amd64.deb
+sudo sed -i.bak 's/;http_port = 3000/http_port = 80/g' /etc/grafana/grafana.ini
+sudo sed -i.bak 's/CapabilityBoundingSet=/CapabilityBoundingSet=CAP_NET_BIND_SERVICE/g' /lib/systemd/system/grafana-server.service
+sudo sed -i.bak  '/Service/a AmbientCapabilities=CAP_NET_BIND_SERVICE' /lib/systemd/system/grafana-server.service
+sudo sed -i.bak '/Service/a PrivateUsers=false' /lib/systemd/system/grafana-server.service
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
